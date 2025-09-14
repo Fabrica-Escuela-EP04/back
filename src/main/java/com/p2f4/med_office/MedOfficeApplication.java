d5c98ba7b9e91afb6f966b2a7ddc833dda8f2fc1
afflorezc <afflorezc@gmail.com andresf.florezc@udea.edu.co>
2025-09-13 19:20:23 -0500

    backend project initialization with spring boot

---
+++ src/main/java/com/p2f4/med_office/MedOfficeApplication.java
@@ -0,0 +1,13 @@
+package com.p2f4.med_office;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedOfficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedOfficeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/p2f4/med_office/MedOfficeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.p2f4.med_office;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedOfficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
