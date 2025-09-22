f0132ffea8d5aa1a9e92760df94db2f41db214a8
afflorezc <afflorezc@gmail.com andresf.florezc@udea.edu.co>
2025-09-21 22:54:25 -0500

    constructor and getters and setters added to ScheduleDTO

--- src/main/java/com/p2f4/med_office/dto/ScheduleDTO.java
+++ src/main/java/com/p2f4/med_office/dto/ScheduleDTO.java
@@ -11,4 +11,66 @@ public class ScheduleDTO {
     private LocalDateTime startDate;
     private LocalDateTime endDate;
 
+    public ScheduleDTO(){
+    }
+
+    public ScheduleDTO(Long idSchedule, Long idUser, String type, Integer idOffice, LocalDateTime startDate,
+            LocalDateTime endDate) {
+        this.idSchedule = idSchedule;
+        this.idUser = idUser;
+        this.type = type;
+        this.idOffice = idOffice;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public Long getIdSchedule() {
+        return idSchedule;
+    }
+
+    public void setIdSchedule(Long idSchedule) {
+        this.idSchedule = idSchedule;
+    }
+
+    public Long getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(Long idUser) {
+        this.idUser = idUser;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Integer getIdOffice() {
+        return idOffice;
+    }
+
+    public void setIdOffice(Integer idOffice) {
+        this.idOffice = idOffice;
+    }
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDateTime getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDateTime endDate) {
+        this.endDate = endDate;
+    }
+
+
 }
